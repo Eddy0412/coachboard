eefc701f5750065c11c319eaa9e648786484fecf
Eddy0412 <eddy04@gmail.com>
2026-04-25 03:34:03 -0500

    CoachIQ: include time in last analyzed label

--- coachboard-pro/components/workspace/coachiq.tsx
+++ coachboard-pro/components/workspace/coachiq.tsx
@@ -30,10 +30,12 @@ interface CoachIQProps {
 }
 
 function formatDate(iso: string) {
-  return new Date(iso).toLocaleDateString("en-US", {
+  return new Date(iso).toLocaleString("en-US", {
     month: "short",
     day: "numeric",
     year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
   });
 }
 
